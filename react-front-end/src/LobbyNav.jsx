d684e20ef9f48c646925008bcea9e4dc91d8e75a
Danny <dongyunrhee@gmail.com>
2019-05-26 23:48:15 +0000

    Added bootstrap CDN and created Lobby Navbar component.

---
+++ react-front-end/src/LobbyNav.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './App.css';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
+class LobbyNav extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+          
+        }
+      }
+
+      render() {
+          return (
+            <Route>
+                <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-dark border-bottom shadow-sm">
+                    <h5 class="my-0 mr-md-auto font-weight-normal text-white"> Cards Against Internet</h5>
+                    <nav class="my-2 my-md-0 mr-md-3">                    
+
+                        <button class="btn btn-outline-light btn-dark btn-lg">Create Room</button>
+                    </nav>
+                </div>
+          </Route>
+          )
+      }
+}
+
+export default LobbyNav;
