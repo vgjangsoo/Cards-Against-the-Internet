253a26d1da1cf7e4cf7e5fa03fd1fd6e9fb8d96f
Danny <dongyunrhee@gmail.com>
2019-05-27 03:31:18 +0000

    Game lobby logo is now redirecting to the homepage when clicked.

--- react-front-end/src/LobbyNav.jsx
+++ react-front-end/src/LobbyNav.jsx
@@ -15,7 +15,7 @@ class LobbyNav extends Component {
           return (
             <Route>
                 <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-dark border-bottom shadow-sm">
-                    <h5 className="my-0 mr-md-auto font-weight-normal text-white"> Cards Against Internet</h5>
+                    <h5 className="my-0 mr-md-auto font-weight-normal text-white"> <Link to="/" style={{color: 'white', textDecoration: 'none'}}> Cards Against the Internet</Link></h5>
                     <nav className="my-2 my-md-0 mr-md-3">                    
 
                         <button onClick={this.props.createRoom} className="btn btn-outline-light btn-dark btn-lg">Create Room</button>
