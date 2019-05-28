8de73ba8943b02a3aebf9638d801c4b9e42c9eb3
Sungil Saul Lee <jangsoo86@gmail.com>
2019-05-28 00:50:12 +0000

    questioner cards can be imported

--- react-front-end/src/QuestionerDeck.jsx
+++ react-front-end/src/QuestionerDeck.jsx
@@ -7,14 +7,49 @@ class QuestionerDeck extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      message: 'Click the button to load data!'
+      message: 'Click the button to load data!',
+      questions: []
     }
   }
 
   fetchQuestionCards() {
     axios.get('/api/cards')
     .then((res) => {
-      console.log(res.data);
+      const deckCards = res.data.message.cards;
+      let questionCards = [];
+      for (let card in deckCards) {
+        if (deckCards[card].isQuestion === true) {
+          questionCards.push(deckCards[card]);
+        }
+      }
+
+      let questions = [];
+
+      const randomNum1 = Math.floor(Math.random() * (questionCards.length));
+      questions.push(questionCards[randomNum1]);
+      let index1 = questionCards.indexOf(questionCards[randomNum1])
+
+      if (index1 > -1) {
+        questionCards.splice(index1, 1);
+      }
+
+      const randomNum2 = Math.floor(Math.random() * (questionCards.length));
+      questions.push(questionCards[randomNum2]);
+      let index2 = questionCards.indexOf(questionCards[randomNum2])
+
+      if (index2 > -1) {
+        questionCards.splice(index2, 1);
+      }
+
+      const randomNum3 = Math.floor(Math.random() * (questionCards.length));
+      questions.push(questionCards[randomNum3]);
+      let index3 = questionCards.indexOf(questionCards[randomNum3])
+
+      if (index3 > -1) {
+        questionCards.splice(index3, 1);
+      }
+
+      this.setState({ questions: questions });
     })
   }
 
@@ -23,9 +58,16 @@ class QuestionerDeck extends Component {
   }
 
   render() {
+    const selectedQuestions = this.state.questions;
+    
     return (
       <div className="Game">
-        <h3>Questioner's Cards</h3>       
+        <h3>Questioner's Cards</h3>
+        {selectedQuestions.map(e => {
+          return (
+            <div key={e.id}>{e.content}</div>
+          )
+        })}    
       </div>
     );
   }
