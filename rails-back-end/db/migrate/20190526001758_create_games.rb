fc61d099c190c2240a389c4e429479a7face716d
Danny <dongyunrhee@gmail.com>
2019-05-29 02:41:24 +0000

    Changed AR migration datas (gathered user_game_infos data with games data)

--- rails-back-end/db/migrate/20190526001758_create_games.rb
+++ rails-back-end/db/migrate/20190526001758_create_games.rb
@@ -1,19 +1,31 @@
 class CreateGames < ActiveRecord::Migration[5.2]
   def change
     create_table :games do |t|
-      t.integer :maxRound
-      t.integer :currentRound
-      t.boolean :isEveryoneDeck
-      t.integer :currentQuestion
-      t.integer :currentAnswer
-      t.integer :maxPlayers
-      t.integer :creator
-      t.integer :currentQuestioner
-      t.integer :roundWinner
-      t.integer :deck_id
-      t.string :gameStatus
+      t.string :theme
+      t.json 'gameState'
 
       t.timestamps
     end
   end
 end
+
+# Old data types (should be in gameState now)      
+# t.integer :maxRound
+# t.integer :currentRound
+# t.boolean :isEveryoneDeck
+# t.integer :currentQuestion
+# t.integer :currentAnswer
+# t.integer :maxPlayers
+# t.integer :creator
+# t.integer :currentQuestioner
+# t.integer :roundWinner
+# t.integer :deck_id
+# t.string :gameStatus
+
+# From user_game_infos table
+# t.integer :user_id
+# t.integer :game_id
+# t.integer :roundPoints
+# t.string :userStatus
+# t.text :hands, array: true, default: []
+# t.integer :selectedCard
