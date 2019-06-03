219da177af59c1a6f2cc15581eeef4b7ec2b0ac1
Sungil Saul Lee <jangsoo86@gmail.com>
2019-06-03 18:20:57 +0000

    enable loading logo for lobby page

--- react-front-end/src/Lobby.jsx
+++ react-front-end/src/Lobby.jsx
@@ -121,14 +121,19 @@ class Lobby extends Component {
     return (
       <div className="App">
         <LobbyNav createRoom={this.openCreateRoomModal} />
-        
+        {!this.state.lobbyState.length 
+            ? 
+            <div className="loader-container">
+              <div className="loader"></div>
+            </div>
+            :
         <div className="container gameLobbyContainer">
           <div className="grid card-deck mb-3 text-center">
             {createdGameRooms.reverse().map(e => {
               return <Gameroom roomInfo={e} key={e.id} roomId={e.id} cable={this.props.cable}/>;
             })}
           </div>
-        </div>
+        </div>}
 
         <div>
           {this.state.showCreateRoomModal ? (
