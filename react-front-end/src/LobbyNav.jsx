2d52738389e5e688bf0a0e7dd9ffd467cdf670e4
Sungil Saul Lee <jangsoo86@gmail.com>
2019-06-04 21:44:35 +0000

    coditional rendering on lobby page as well.

--- react-front-end/src/LobbyNav.jsx
+++ react-front-end/src/LobbyNav.jsx
@@ -15,7 +15,7 @@ class LobbyNav extends Component {
         <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-dark border-bottom shadow-sm">
           <h5 className="my-0 mr-md-auto font-weight-normal text-white"> <Link className="navbar-logo" to="/" style={{color: 'white', textDecoration: 'none'}}> Cards Against Internet</Link></h5>
           <nav className="my-2 my-md-0 mr-md-3"> 
-          <a className="navbar-a p-2 text-white" href="#" style={{color: 'white', textDecoration: 'none'}}>Logged in as: {this.props.userData.username}</a>                   
+          <a className="navbar-a p-2 text-white" style={{color: 'white', textDecoration: 'none'}}>{this.props.userData.username ? `Logged in as: ${this.props.userData.username}` : null}</a>                   
             <button className="btn btn-dark sign-up-button btn-md nav-signup-button" onClick={this.props.onLogout}>Log Out</button>
             <button onClick={this.props.createRoom} className="btn btn-dark btn-lg create-room-button">Create Room</button>
           </nav>
