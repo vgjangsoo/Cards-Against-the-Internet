cd94f46d79ed6b5bb13796f1dd12be369a51b285
Danny <dongyunrhee@gmail.com>
2019-06-05 00:15:08 +0000

    Modified all buttons and now gameroom is fully responsive at all resolutions

--- react-front-end/src/Game.jsx
+++ react-front-end/src/Game.jsx
@@ -52,9 +52,8 @@ class Game extends Component {
     this.QuestionArea = this.QuestionArea.bind(this);
     this.onSelectAnswer = this.onSelectAnswer.bind(this);
     this.onSelectQuestion = this.onSelectQuestion.bind(this);
-    this.handlerPlayQuestion = this.handlerPlayQuestion.bind(this)
-    this.handlerPlayAnswer = this.handlerPlayAnswer.bind(this)
-
+    this.handlerPlayQuestion = this.handlerPlayQuestion.bind(this);
+    this.handlerPlayAnswer = this.handlerPlayAnswer.bind(this);
   }
 
   componentDidMount() {
@@ -108,28 +107,33 @@ class Game extends Component {
     });
   }
 
-  handlerPlayQuestion(){
-    console.log('question button pressed')
+  handlerPlayQuestion() {
+    console.log("question button pressed");
     const question = this.state.selectedQuestion;
-    console.log('question is:', question)
+    console.log("question is:", question);
     const gameRoomId = this.props.match.params.id;
-    const type = 'question-card-selected'
+    const type = "question-card-selected";
     const gameState = this.state.gameTable.gameState;
-    const userID = this.props.userData.id
+    const userID = this.props.userData.id;
     // const userIndex = this.state.userIndex
-  
-    axios.put(`${API_ROOT}/games/${gameRoomId}?question=${question}&userID=${userID}`, {
-      type: type,
-      gameState: gameState
-    }).then(res =>{
-      console.log('PUT handlerPlayQuestion successful, res:', res)
-    });
+
+    axios
+      .put(
+        `${API_ROOT}/games/${gameRoomId}?question=${question}&userID=${userID}`,
+        {
+          type: type,
+          gameState: gameState
+        }
+      )
+      .then(res => {
+        console.log("PUT handlerPlayQuestion successful, res:", res);
+      });
 
     //need to clear both selectedAnswer and selectedQuestion after posting?
   }
 
-  handlerPlayAnswer(){
-    console.log('answer button pressed')
+  handlerPlayAnswer() {
+    console.log("answer button pressed");
 
     //need to clear both selectedAnswer and selectedQuestion after posting
   }
@@ -152,7 +156,13 @@ class Game extends Component {
     // should have AnswerDeck on top, AnswerSection on bottom
 
     if (!isStartMode) {
-      return <AnswerSection userStatus={gameTable.gameState.playersInfo} currentQuestioner={gameTable.gameState.gameInfo.currentQuestioner} maxPlayers={gameTable.maxPlayers}/>
+      return (
+        <AnswerSection
+          userStatus={gameTable.gameState.playersInfo}
+          currentQuestioner={gameTable.gameState.gameInfo.currentQuestioner}
+          maxPlayers={gameTable.maxPlayers}
+        />
+      );
     }
     let activeUserInfo = "";
     const numPlayers = this.state.gameTable.gameState.gameInfo.currentPlayers;
@@ -161,7 +171,10 @@ class Game extends Component {
 
     for (let i = 0; i <= numPlayers - 1; i++) {
       //trying to find the ONE player in playersInfo.user array and also check if they are questioner
-      if (this.props.userData.id ===this.state.gameTable.gameState.playersInfo.users[i].id) {
+      if (
+        this.props.userData.id ===
+        this.state.gameTable.gameState.playersInfo.users[i].id
+      ) {
         activeUserInfo = this.state.gameTable.gameState.playersInfo.users[i];
       }
     }
@@ -170,11 +183,11 @@ class Game extends Component {
       debugger;
     }
 
-    //trying to only render the QuestionDeck or Question section based on activeUserInfo 
-    let isAnswerer = activeUserInfo.answerCards.length > 0
-    //check again if this person should show  
-    if (questionerID === this.props.userData.id){
-      isAnswerer = false;  
+    //trying to only render the QuestionDeck or Question section based on activeUserInfo
+    let isAnswerer = activeUserInfo.answerCards.length > 0;
+    //check again if this person should show
+    if (questionerID === this.props.userData.id) {
+      isAnswerer = false;
     }
     console.log("ANSWERS activeUserInfo:", activeUserInfo);
     //conditionally render based on:
@@ -183,7 +196,7 @@ class Game extends Component {
     return (
       <div>
         {isAnswerer ? (
-          <AnswererDeck gameState={gameState} activeUserInfo={activeUserInfo}/>
+          <AnswererDeck gameState={gameState} activeUserInfo={activeUserInfo} />
         ) : (
           <AnswerSection
             userStatus={gameTable.gameState.playersInfo}
@@ -203,7 +216,7 @@ class Game extends Component {
       return <QuestionSection />;
     }
     let activeUserInfo;
-    const numPlayers = this.state.gameTable.gameState.gameInfo.currentPlayers
+    const numPlayers = this.state.gameTable.gameState.gameInfo.currentPlayers;
     // let tempUserIndex = -1;
     // let usersArray = this.state.gameTable.gameState.playersInfo.users
 
@@ -215,8 +228,11 @@ class Game extends Component {
 
     for (let i = 0; i <= numPlayers - 1; i++) {
       //trying to find the ONE player in playersInfo.user array
-      if (this.props.userData.id === this.state.gameTable.gameState.playersInfo.users[i].id){
-        activeUserInfo = this.state.gameTable.gameState.playersInfo.users[i]
+      if (
+        this.props.userData.id ===
+        this.state.gameTable.gameState.playersInfo.users[i].id
+      ) {
+        activeUserInfo = this.state.gameTable.gameState.playersInfo.users[i];
         // tempUserIndex = i;
         break;
       }
@@ -228,13 +244,13 @@ class Game extends Component {
     // tempUserIndex = usersArray.findIndex(findUserIndex);
     // this.setState({userIndex: tempUserIndex})
 
-    //trying to only render the QuestionDeck or Question section based on activeUserInfo 
-    let isQuestioner = activeUserInfo.questionCards.length > 0
-    let currentStatus = this.state.gameTable.gameState.gameInfo.status
-    if (currentStatus === 'Question selected, please choose an answer'){
+    //trying to only render the QuestionDeck or Question section based on activeUserInfo
+    let isQuestioner = activeUserInfo.questionCards.length > 0;
+    let currentStatus = this.state.gameTable.gameState.gameInfo.status;
+    if (currentStatus === "Question selected, please choose an answer") {
       isQuestioner = true;
     }
-    
+
     //set the userIndex inside the gameState object to be passed to backend
     // if(tempUserIndex >= 0){
     //   console.log('tempUserIndex is:',tempUserIndex)
@@ -278,22 +294,22 @@ class Game extends Component {
               <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 border-bottom shadow-sm nav-bar-in-game">
                 <div className="my-0 mr-md-auto font-weight-normal">
                   <h6 className="game-number">Room: {gameTable.id} </h6>
-                  <h6 className="game-logged-ID">
-                    Logged in: {this.props.userData.username}/ ID:{this.props.userData.id}{" "}
-                  </h6>
-
                   <h3 className="game-theme"> {gameTable.theme}</h3>
                 </div>
-                <nav className="my-2 my-md-1 mr-md-3 game-round">
-                  <div className="p-5">
-                    <h6>
+                <nav className="my-2 my-md-1 mr-md-3">
+                  <div className="p-5 gameroom-info">
+                    <h6 className="game-round">
                       Round: {gameTable.gameState.gameInfo.currentRound} /{" "}
                       {gameTable.maxRound}
                     </h6>
-                    <h6>
+                    <h6 className="game-player">
                       Players: {gameTable.gameState.gameInfo.currentPlayers} /{" "}
                       {gameTable.maxPlayers}
                     </h6>
+                    <h6 className="game-logged-ID">
+                    Logged in: {this.props.userData.username}/ ID:
+                    {this.props.userData.id}{" "}
+                  </h6>
                   </div>
                 </nav>
                 <nav className="my-2 my-md-1 mr-md-3">
@@ -329,18 +345,31 @@ class Game extends Component {
                     <div className="questioner shadow-lg" style={style}>
                       {this.QuestionArea(gameTable.gameState)}
                     </div>
+
                     <div className="status-message">
+                      <div>
+                        <button
+                          className="btn btn-dark btn-md p-2 game-status-button"
+                          onClick={this.handlerPlayQuestion}
+                        >
+                          Play Q Card
+                        </button>
+                      </div>
+                      <div>
                       {
                         <h6 className="game-status-message">
                           {gameTable.gameState.gameInfo.status}
                         </h6>
                       }
                     </div>
-                    <div>
-                      <button className='btn btn-dark btn-md p-2 game-status-button' onClick={this.handlerPlayQuestion} >Play Q Card</button>
-                    </div>
-                    <div className='play-card-button'>
-                      <button className='btn btn-dark btn-md p-2 game-status-button' onClick={this.handlerPlayAnswer} >Play A Card</button>
+                      <div className="play-card-button">
+                        <button
+                          className="btn btn-dark btn-md p-2 game-status-button"
+                          onClick={this.handlerPlayAnswer}
+                        >
+                          Play A Card
+                        </button>
+                      </div>
                     </div>
                     <div className="answerers shadow-lg" style={style}>
                       {this.AnswerArea(gameTable.gameState, gameTable)}
@@ -353,7 +382,7 @@ class Game extends Component {
                       /> */}
                     </div>
                     <br />
-                    <div className="chat-history-bar">
+                    <div className="chat-history-bar" >
                       <div>
                         <History />
                       </div>
