e5c1ee1f289ed71b6c26e2faaf4b8bdc714cba67
Danny <dongyunrhee@gmail.com>
2019-06-05 01:46:11 +0000

    When not logged in, log-out button not showing up in game lobby

--- react-front-end/src/LobbyNav.jsx
+++ react-front-end/src/LobbyNav.jsx
@@ -1,26 +1,55 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import './css/App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./css/App.css";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 class LobbyNav extends Component {
   constructor(props) {
-    super(props)
-    this.state = {}
+    super(props);
+    this.state = {};
   }
 
   render() {
     return (
       <Route>
         <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-dark border-bottom shadow-sm">
-          <h5 className="my-0 mr-md-auto font-weight-normal text-white"> <Link className="navbar-logo" to="/" style={{color: 'white', textDecoration: 'none'}}> Cards Against Internet</Link></h5>
-          <nav className="my-2 my-md-0 mr-md-3 lobby-nav-container"> 
-          <a className="navbar-a p-2 text-white" style={{color: 'white', textDecoration: 'none'}}>{this.props.userData.username ? `Logged in as: ${this.props.userData.username}` : null}</a>                   
-            <button className="btn btn-dark sign-up-button btn-md create-room-button" onClick={this.props.onLogout}>Log Out</button>
-            <button onClick={this.props.createRoom} className="btn btn-dark btn-lg create-room-button">Create Room</button>
+          <h5 className="my-0 mr-md-auto font-weight-normal text-white">
+            {" "}
+            <Link
+              className="navbar-logo"
+              to="/"
+              style={{ color: "white", textDecoration: "none" }}
+            >
+              {" "}
+              Cards Against Internet
+            </Link>
+          </h5>
+          <nav className="my-2 my-md-0 mr-md-3 lobby-nav-container">
+            <a
+              className="navbar-a p-2 text-white"
+              style={{ color: "white", textDecoration: "none" }}
+            >
+              {this.props.userData.username
+                ? `Logged in as: ${this.props.userData.username}`
+                : null}
+            </a>
+            {this.props.userData.username ? (
+              <button
+                className="btn btn-dark sign-up-button btn-md create-room-button"
+                onClick={this.props.onLogout}
+              >
+                Log Out
+              </button>
+            ) : null}
+            <button
+              onClick={this.props.createRoom}
+              className="btn btn-dark btn-lg create-room-button"
+            >
+              Create Room
+            </button>
           </nav>
         </div>
-    </Route>
+      </Route>
     );
   }
 }
