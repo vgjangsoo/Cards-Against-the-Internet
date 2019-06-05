566cadb892264f80d9b547321cf9526df5aaf69c
Danny <dongyunrhee@gmail.com>
2019-06-05 04:10:06 +0000

    Fixed UserNotLoggedIn.jsx/css file errors

--- react-front-end/src/UserNotLoggedIn.jsx
+++ react-front-end/src/UserNotLoggedIn.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import "./css/App.css";
 import "./css/UserNotLoggedIn.css"
 
 class UserNotLoggedIn extends Component {
@@ -9,12 +8,12 @@ class UserNotLoggedIn extends Component {
 
     return (
       <div className="UserNotLoggedIn">
-        <img className='card-img answerer-img' src="http://depp5bqkz634d.cloudfront.net/car-manager/demo3/wp-content/themes/car-manager/images/not-login-icon.png" alt="Not-logged-in" style={{width: "100%", height: "100%"}}/>
+        <img className='page-img' src="http://depp5bqkz634d.cloudfront.net/car-manager/demo3/wp-content/themes/car-manager/images/not-login-icon.png" alt="Not-logged-in" style={{width: "100%", height: "100%"}}/>
         <br/>
         <br/>
-        <h2>Please Login.</h2>
+        <h2 className="page-header">Please Login.</h2>
         <hr/>
-        <h5 className="text-white"><Link className="navbar-logo responsive-logo" to="/"> Cards Against Internet</Link> </h5>
+        <h5><Link className="link-to-home btn" to="/" style={{textDecoration: 'none'}}> Cards Against Internet</Link> </h5>
       </div>
     );
   }
