0f938f74122b9a02f830710c5d8e1fabe19e1887
Sungil Saul Lee <jangsoo86@gmail.com>
2019-06-05 18:46:17 +0000

    Add cors to project and and test with local ip

--- react-front-end/src/constants/index.js
+++ react-front-end/src/constants/index.js
@@ -1,5 +1,5 @@
-export const API_ROOT = 'http://localhost:3000/api';
-export const API_WS_ROOT = 'ws://localhost:3000/cable';
+export const API_ROOT = 'http://192.168.88.173:3000/api';
+export const API_WS_ROOT = 'ws://192.168.88.173:3000/cable';
 export const HEADERS = {
   'Content-Type': 'application/json',
   Accept: 'application/json',
